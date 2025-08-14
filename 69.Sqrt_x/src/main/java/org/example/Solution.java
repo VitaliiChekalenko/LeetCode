38147d4c376c976a3e659dd3933352f6d95744f6
Vitalii <chekalenkovitaliy@gmail.com>
2025-08-14 11:45:54 +0200

    69. Sqrt(x)

--- 69.Sqrt_x/src/main/java/org/example/Solution.java
+++ 69.Sqrt_x/src/main/java/org/example/Solution.java
@@ -11,15 +11,15 @@ class Solution {
         while (start <= end) {
             mid = start + (end - start) / 2;
 
-            if (((long) (mid * mid)) > ((long) x)) {
+            if ((long) mid * mid > (long) x) {
                 end = mid - 1;
-            } else if (((long) (mid * mid)) == ((long) x)) {
+            } else if (mid * mid == x) {
                 return mid;
             } else {
                 start = mid + 1;
             }
         }
 
-        return Math.round(end);
+        return end;
     }
 }
