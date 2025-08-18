77516d28464e948cd64410924866bfac772899ec
Vitalii <chekalenkovitaliy@gmail.com>
2025-08-18 15:54:47 +0200

    383. Ransom Note

--- 383.RansomNote/src/main/java/org/example/Solution.java
+++ 383.RansomNote/src/main/java/org/example/Solution.java
@@ -1,38 +1,21 @@
 package org.example;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicBoolean;
-
 class Solution {
-        public boolean canConstruct(String ransomNote, String magazine) {
-            if (ransomNote.length() > magazine.length()) return false;
+    public boolean canConstruct(String ransomNote, String magazine) {
+        if (ransomNote.length() > magazine.length()) return false;
+        int[] alphabet = new int[26];
 
-            Map<Character, Integer> ransonMap = new HashMap<>();
-            Map<Character, Integer> magazineMap = new HashMap<>();
-
-            getMap(ransomNote, ransonMap);
-            getMap(magazine, magazineMap);
-
-            AtomicBoolean res = new AtomicBoolean(true);
-
-            ransonMap.keySet().forEach(character -> {
-                if (magazineMap.get(character) == null && magazineMap.get(character) < ransonMap.get(character)) {
-                    res.set(false);
-                }
-            });
-
-            return true;
+        for (char c : magazine.toCharArray()) {
+            alphabet[c - 'a']++;
         }
 
-    private void getMap(String ransomNote, Map<Character, Integer> ransonMap) {
-        for (int i = 0; i < ransomNote.length(); i++) {
-            char charAt = ransomNote.charAt(i);
-            if (!ransonMap.containsKey(charAt)) {
-                ransonMap.put(charAt, 1);
-            } else {
-                ransonMap.put(charAt, ransonMap.get(charAt) + 1);
+        for (char c : ransomNote.toCharArray()) {
+            if (alphabet[c - 'a'] == 0) {
+                return false;
             }
+            alphabet[c - 'a']--;
         }
+
+        return true;
     }
 }
