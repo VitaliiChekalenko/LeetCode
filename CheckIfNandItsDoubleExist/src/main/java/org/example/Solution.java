4686355abaf85ebf36676701b4aa93ddbb4e9a60
Vitalii <chekalenkovitaliy@gmail.com>
2025-08-19 11:48:44 +0200

    Check If N and Its Double Exist

---
+++ CheckIfNandItsDoubleExist/src/main/java/org/example/Solution.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+
+    public boolean checkIfExist(int[] arr) {
+        Set<Integer> seen = new HashSet<>();
+
+        for (int num : arr) {
+            if (seen.contains(2 * num) ||
+                    (num % 2 == 0 && seen.contains(num / 2))) {
+                return true;
+            }
+            seen.add(num);
+        }
+        return false;
+    }
+}
