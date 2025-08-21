a7d536c1ecede83a7ea2ec4b4e413f283c579201
Vitalii <chekalenkovitaliy@gmail.com>
2025-08-22 00:18:16 +0200

    112. Path Sum

--- 112PathSum/src/main/java/org/example/Solution.java
+++ 112PathSum/src/main/java/org/example/Solution.java
@@ -4,7 +4,6 @@ class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
         if (root == null) return false;
 
-        // Якщо це листок — перевіряємо суму
         if (root.left == null && root.right == null) {
             return targetSum == root.val;
         }
