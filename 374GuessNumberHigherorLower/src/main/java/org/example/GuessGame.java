47d2d4e4c9372fdf8f0dc68615382af1900a2919
Vitalii <chekalenkovitaliy@gmail.com>
2025-08-23 14:55:21 +0200

    374. Guess Number Higher or Lower

---
+++ 374GuessNumberHigherorLower/src/main/java/org/example/GuessGame.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class GuessGame {
+    public int guess(int num) {
+        return 0;
+    }
+}
---
+++ 374GuessNumberHigherorLower/src/main/java/org/example/Solution.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int left = 0, right = n;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            int guessed = guess(mid);
+            if (guessed == 0) {
+                return mid;
+            } else if (guessed == 1) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+}
